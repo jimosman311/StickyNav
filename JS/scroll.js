40ecf73106fd608fc4c029d3ef38d69c75d7b8d7
jimosman311 <jimosman311@gmail.com>
2019-12-16 16:50:19 +0800

    Added Scroll To Top functionality

--- JS/scroll.js
+++ JS/scroll.js
@@ -1,3 +1,4 @@
+//Stickynav
 $(window).on('scroll', function() {
     if($(window).scrollTop() > 0) {
         $('nav').addClass('black');
@@ -5,3 +6,35 @@ $(window).on('scroll', function() {
         $('nav').removeClass('black');
     }
 });
+
+//Scroll To Top fucntionality
+(function(){
+    let button;
+    let toggleScrollToTop = function() {
+        if($('html').scrollTop() > 0 || $('body').scrollTop() >0) {
+            button.fadeIn(400);
+        } else {
+            button.fadeOut(400);
+        }
+    };
+
+    $(document).ready(function() {
+        button = $(".Scroll-To-Top");
+
+        $(window).scroll(toggleScrollToTop);
+
+        toggleScrollToTop();
+
+        //Click to scroll to the top
+
+        button.on('click', function() {
+            $('html').animate({
+                scrollTop:0
+            },900);
+
+            $('body').animate({
+                scrollTop:0
+            },900);
+        });
+    });
+})();
